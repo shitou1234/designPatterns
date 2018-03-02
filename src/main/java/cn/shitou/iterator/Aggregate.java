5cb9107dc1b7a498aceaf1394a36330871e6dfea
shijinrui <shijinrui@server>
2018-03-02 09:20:27 +0800

    first commit

---
+++ src/main/java/cn/shitou/iterator/Aggregate.java
@@ -0,0 +1,10 @@
+package cn.shitou.iterator;
+
+import java.util.Iterator;
+
+/**
+ * Created by Administrator on 2018/1/29.
+ */
+public interface Aggregate {
+    public abstract Iterator iterator();
+}
